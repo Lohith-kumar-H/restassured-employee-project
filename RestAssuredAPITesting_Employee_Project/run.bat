7d9464b542b29cd449c2eb860006086f159bb867
Lohith-kumar-H <lohithgowda3636@gmail.com>
2022-08-16 15:55:09 +0530

    this my first commit...

---
+++ RestAssuredAPITesting_Employee_Project/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\ADMIN\Desktop\restassured\RestAssuredAPITesting_Employee_Project
+mvn clean install
---
+++ RestAssuredAPITesting_Employee_Project/src/test/java/employee/base/TestBase.java
@@ -0,0 +1,27 @@
+package employee.base;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.response.Response;	
+import io.restassured.specification.RequestSpecification;
+
+public class TestBase
+{
+	public static RequestSpecification  httpRequest;
+	public static Response response;
+	public String empID="16"; //hard coded - Input for get details of single Employee and update employee
+	
+	public Logger logger;
+	
+	@BeforeClass 
+	public void setup()
+	{
+		logger=Logger.getLogger("EmployeeRestAPI");//adder Logger
+		PropertyConfigurator.configure("log4j.properties");//adder Logger
+		logger.setLevel(Level.DEBUG);
+	}
+
+}
